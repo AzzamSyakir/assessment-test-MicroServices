9c59ebf7ff74b288d85e028c1dbd777f8f7a7684
Azzam Syakir <azzamsykir@gmail.com>
2024-07-18 13:43:22 +0700

    fix: fixed MongoDB setup docker

--- db/MongoDB/mongo-init.js
+++ db/MongoDB/mongo-init.js
@@ -1,20 +1,20 @@
-var dbUser = process.env.MONGO_USER;
-var dbPassword = process.env.MONGO_PASSWORD;
-db = db.getSiblingDB("appDb")
+db = db.getSiblingDB("appDb");
 
-
-db.createUser({
-    user: dbUser,
-    pwd: dbPassword,
-    roles: [
-      {
-        role: 'dbOwner',
-      db: 'appDb',
-    },
-  ],
-}); 
 db.createCollection("accounts");
+print("Collection 'accounts' created");
+
 db.createCollection("users");
+print("Collection 'users' created");
+
 db.createCollection("offices");
+print("Collection 'offices' created");
+
 db.createCollection("roles");
+print("Collection 'roles' created");
+
 db.createCollection("layers");
+print("Collection 'layers' created");
+
+print("init script finish");
+
+
